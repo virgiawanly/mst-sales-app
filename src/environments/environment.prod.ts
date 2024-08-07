44a01a7a2870470257bf12adbe865fed9d54face
Virgiawan Listiyandi <listiyandiv@gmail.com>
2024-08-07 23:44:32 +0700

    chore: change api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'https://mst-sales-api.virgiawan.dev/api',
+  api_url: 'https://mst-api.virgiawan.dev/api',
   api_token_identifier: 'mstSales@apiToken',
 };
